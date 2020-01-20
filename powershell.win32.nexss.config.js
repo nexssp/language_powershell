44c037c12a8a82689d49d2888f527b21f99cf3a8
Marcin Polak <mapoart@gmail.com>
2020-01-20 09:16:10 +0100

    Added Bypass policy

--- powershell.win32.nexss.config.js
+++ powershell.win32.nexss.config.js
@@ -3,29 +3,15 @@ languageConfig.title = "PowerShell";
 languageConfig.description = "Windows Power Shell script";
 languageConfig.url = "https://www.microsoft.com/";
 languageConfig.extensions = [".ps1"];
-languageConfig.builders = {
-  tofill: {
-    install: "scoop install llvm",
-    //build: "pkg --output <destinationFile> --out-path <destinationPath> <file>",
-    command: "clang++",
-    build: function() {
-      const path = require("path");
-      //take command from current folder
-      return path.join(__dirname, "customCompiler.win32.powershell.cmd");
-    },
-    args: "<file> <destinationFile>",
-    // C++ needs to be build to exe, so no compile option
-    help: ``
-  }
-};
+languageConfig.builders = {};
 languageConfig.compilers = {
-  ps1: {
-    install: "installed.",
-    // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
-    command: "PowerShell.exe",
-    args: "-File <file>",
-    help: ``
-  }
+    ps1: {
+        install: "installed.",
+        // Cpp does not have possibility to compile and run on the fly. We need to save it as a exe file first.
+        command: "PowerShell.exe",
+        args: "-ExecutionPolicy ByPass -File <file>",
+        help: ``
+    }
 };
 languageConfig.errors = require("./nexss.powershell.errors");
 languageConfig.languagePackageManagers = {};
