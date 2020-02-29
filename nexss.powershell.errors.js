8cbaf90809e9b8345fdb24217d94e298ada524e7
Marcin Polak <mapoart@gmail.com>
2020-02-29 11:45:15 +0100

    Added error handling for parameters

--- nexss.powershell.errors.js
+++ nexss.powershell.errors.js
@@ -1,7 +1,4 @@
 module.exports = {
-  win32: {
-    "ModuleNotFoundError: No module named '(.*?)'":
-      "nexss install py <module> OR pip3 install <module>",
-    "not found": `nexss install py <module> OR pip3 install <module>`
-  }
+  "Cannot process command because of one or more missing mandatory parameters: (?<found1>.*?)\\.":
+    "There are some arguments mendatory in this script: <found1>"
 };
