fd72115e570a789bf7bece8822282960fd933abe
Marcin Polak <mapoart@gmail.com>
2020-03-05 10:54:52 +0100

    Added authors, developers, year..

--- powershell.win32.nexss.config.js
+++ powershell.win32.nexss.config.js
@@ -1,16 +1,19 @@
 let languageConfig = Object.assign({}, require("../config.win32"));
 languageConfig.title = "PowerShell";
 languageConfig.description = "Windows Power Shell script";
-languageConfig.url = "https://www.microsoft.com/";
+languageConfig.url = "https://www.microsoft.com";
+languageConfig.founders = ["Microsoft"];
+languageConfig.developers = [""];
+languageConfig.years = ["2006"];
 languageConfig.extensions = [".ps1"];
 languageConfig.builders = {};
 languageConfig.compilers = {
-    ps1: {
-        install: "installed.",
-        command: "PowerShell.exe",
-        args: "-ExecutionPolicy ByPass -File <file>",
-        help: ``
-    }
+  ps1: {
+    install: "installed.",
+    command: "PowerShell.exe",
+    args: "-ExecutionPolicy ByPass -File <file>",
+    help: ``
+  }
 };
 languageConfig.errors = require("./nexss.powershell.errors");
 languageConfig.languagePackageManagers = {};
