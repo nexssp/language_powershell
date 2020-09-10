7fc336192406afd9c55451c3fbb75b6e9f21c735
Marcin Polak <mapoart@gmail.com>
2020-09-10 13:53:11 +0200

    Different powershell installer for Arch Linux

--- powershell.linux.nexss.config.js
+++ powershell.linux.nexss.config.js
@@ -19,11 +19,26 @@ if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
     replaceCommandByDist,
     dist,
   } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
-
+  const distName = dist();
+  switch (distName) {
+    case "Arch Linux":
+      languageConfig.compilers.Pwsh.install = `wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-x64.tar.gz
+pacman -Syy 
+pacman -S --noconfirm core/icu openssl-1.0
+installFolder="/usr/src/powershell-7"
+mkdir -p "$installFolder"
+tar zxf powershell-7.0.3-linux-x64.tar.gz -C "$installFolder"
+rm -f /tmp/powershell*.tar.gz
+ln -s ${installFolder}/pwsh /usr/bin/pwsh
+pacman -Scc`;
+      break;
+    default:
+      languageConfig.compilers.Pwsh.install = replaceCommandByDist(
+        languageConfig.compilers.Pwsh.install
+      );
+      break;
+  }
   // This function just replace all apt-get,apt to the right distribution pkg installer.
-  languageConfig.compilers.Pwsh.install = replaceCommandByDist(
-    languageConfig.compilers.Pwsh.install
-  );
 
   languageConfig.dist = dist();
 }
