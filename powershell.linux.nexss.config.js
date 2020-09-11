913916dc957782c26a7d03e7e6f13dad09d9d49d
Marcin Polak <mapoart@gmail.com>
2020-09-11 11:44:46 +0200

    better installer for linux

--- powershell.linux.nexss.config.js
+++ powershell.linux.nexss.config.js
@@ -2,16 +2,34 @@ let languageConfig = Object.assign(
   {},
   require(`./powershell.win32.nexss.config`)
 );
+
+let sudo = "sudo ";
+if (process.getuid && process.getuid() === 0) {
+  sudo = "";
+}
+
 languageConfig.compilers = {
   Pwsh: {
-    install:
-      "apt install wget && wget -O - https://aka.ms/install-powershell.sh | bash",
+    install: `${sudo}apt install -y wget && wget -O - https://aka.ms/install-powershell.sh | ${sudo}bash`,
     command: "pwsh",
     args: "-ExecutionPolicy ByPass -File <file>",
     help: ``,
   },
 };
 languageConfig.errors = require("./nexss.powershell.errors");
+
+const  getPowershellInstaller = (pre, post, version = "7.0.3") => {
+  return `${pre} core/icu openssl-1.0 wget
+wget https://github.com/PowerShell/PowerShell/releases/download/v${version}/powershell-${version}-linux-x64.tar.gz
+installFolder="/usr/src/powershell"
+mkdir -p "$installFolder"
+tar zxf powershell-${version}-linux-x64.tar.gz -C "$installFolder"
+rm -f ./powershell*.tar.gz
+ln -s "$installFolder"/pwsh /usr/bin/pwsh
+${post}`;
+}
+
+const powershellInstall = 
 
 // If statement must be here for older versions nexss <2.1.12
 if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
@@ -20,17 +38,16 @@ if (require("fs").existsSync(`${process.env.NEXSS_SRC_PATH}/lib/osys.js`)) {
     dist,
   } = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
   const distName = dist();
+
+  
+
   switch (distName) {
     case "Arch Linux":
-      languageConfig.compilers.Pwsh.install = `pacman -Syy
-pacman -S --noconfirm core/icu openssl-1.0 wget
-wget https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-x64.tar.gz
- installFolder="/usr/src/powershell-7"
-mkdir -p "$installFolder"
-tar zxf powershell-7.0.3-linux-x64.tar.gz -C "$installFolder"
-rm -f ./powershell*.tar.gz
-ln -s "$installFolder"/pwsh /usr/bin/pwsh
-pacman -Scc`;
+      languageConfig.compilers.Pwsh.install = getPowershellInstaller(`${sudo}pacman -Syy
+${sudo}pacman -S --noconfirm`,`${sudo}pacman -Scc`,"7.0.3");
+    case "Alpine Linux":
+      languageConfig.compilers.Pwsh.install = getPowershellInstaller(`${sudo}apt-get update
+${sudo}apt-get install -y`,`${sudo}apt-get autoremove`,"7.0.3");
       break;
     default:
       languageConfig.compilers.Pwsh.install = replaceCommandByDist(
