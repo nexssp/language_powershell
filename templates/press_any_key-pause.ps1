752a3e4a0d6e08409aa760b80e2e2944f80af145
Marcin Polak <mapoart@gmail.com>
2021-03-23 08:04:28 +0100

    NEW TEMPLATE: press any key/pause function.

---
+++ templates/press_any_key-pause.ps1
@@ -0,0 +1,14 @@
+# https://stackoverflow.com/questions/20886243/press-any-key-to-continue
+Function pause ($message) {
+    # Check if running Powershell ISE
+    if ($psISE) {
+        Add-Type -AssemblyName System.Windows.Forms
+        [System.Windows.Forms.MessageBox]::Show("$message")
+    }
+    else {
+        Write-Host "$message" -ForegroundColor Yellow
+        $x = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    }
+}
+
+pause "Press any key to continue"
